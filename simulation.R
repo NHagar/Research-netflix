a134fec6f2117cbf6e400efc4fccb4a47d2f93d3
NHagar <nicholasrhagar@gmail.com>
2021-07-21 16:53:35 -0500

    Fixed popularity simulation

--- simulation.R
+++ simulation.R
@@ -3,10 +3,14 @@ source("./R/sim_functions.R")
 
 set.seed(20210721)
 
+# ----Parameters----
 n_movies <- 500
 movies <- seq(1, n_movies)
 users <- 500000
 days <- 21
+
+alpha <- 0.1
+beta <- 3
 
 # ----Random----
 results <- matrix(nrow=days, ncol=users)
@@ -20,7 +24,32 @@ df <- transform_results(results)
 plots <- plot_results(df)
 
 # ----Popularity, fixed----
+results <- matrix(nrow=days, ncol=users)
 
+for (i in seq(1, days)) {
+  # Generate skewed draw probabilities
+  p <- sort(rbeta(n_movies, alpha, beta), decreasing=T)
+  
+  if (i==1) {
+    # Shuffle movie order to start
+    movies_sorted <- sample(movies)
+  }
+  
+  # Weighted user draw
+  s <- sample(movies_sorted, users, replace=T, prob=p)
+  # Stash in results matrix
+  results[i,] <- s
+  # Get new order based on choices
+  movies_sorted <- as_tibble(choices) %>% 
+    group_by(value) %>% 
+    summarize(count=n()) %>% 
+    arrange(desc(count)) %>% 
+    pull(value)
+  movies_sorted <- c(movies_sorted, movies[!movies %in% movies_sorted])
+}
+
+df <- transform_results(results)
+plots <- plot_results(df)
 
 
 # ----Popularity, variable----
