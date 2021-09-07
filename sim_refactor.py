74b20d654a6e2cce5d0881168f42af95be30012c
NHagar <nicholasrhagar@gmail.com>
2021-09-07 13:51:26 -0500

    initial round of user selections

--- sim_refactor.py
+++ sim_refactor.py
@@ -1,26 +1,43 @@
 # %%
+import collections
+
 import numpy as np
 from scipy import stats
 
+from tqdm import tqdm
 # %%
-n_users = 1000
+n_users = 200_000_000
 n_movies = 100
 n_days = 30
 pop_val = 0.5
 # %%
 users = stats.uniform.rvs(size=n_users)
 movies = stats.uniform.rvs(size=n_movies)
+tallies = [dict() for i in range(n_days)]
+
 # %%
-choices = ((stats.binom.rvs(1, 1-pop_val, size=n_days)==1)).astype("object")
-u = users[0]
-local = np.count_nonzero(choices==0)
+def make_local_choices(user, items, local_count):
+    local_choices = []
+    dist = abs(items - user)
+    for i in range(local_count):
+        local_choices.append(dist.argmin())
+        dist = np.delete(dist, dist.argmin())
+    
+    return local_choices
+
+
 # %%
-local_choices = []
-dist = abs(movies - u)
-for i in range(local):
-    local_choices.append(dist.argmin())
-    dist = np.delete(dist, dist.argmin())
+for u in tqdm(users):
+    choices = ((stats.binom.rvs(1, 1-pop_val, size=n_days)==1)).astype("object")
+    local = np.count_nonzero(choices==0)
+    local_choices = make_local_choices(u, movies, local)
+    choices[choices==False] = np.array(local_choices)
+
+    for i in range(n_days):
+        k = choices[i]
+        if k in tallies[i]:
+            tallies[i][k] += 1
+        else:
+            tallies[i][k] = 1
+
 # %%
-choices[choices==False] = np.array(local_choices)
-# %%
-choices
