91885de10e437391b47d3e57a3b135d5dd39219c
NHagar <nicholasrhagar@gmail.com>
2021-09-10 13:05:55 -0500

    iteration bug fix

--- sim_refactor.py
+++ sim_refactor.py
@@ -1,24 +1,16 @@
 # %%
 import numpy as np
+import pandas as pd
 from scipy import stats
 
 from tqdm import tqdm
-# %%
-n_users = 1_000
-n_movies = 100
-n_days = 30
-pop_val = 0.5
-pl_val = 1.5
-# %%
-users = stats.uniform.rvs(size=n_users)
-movies = stats.uniform.rvs(size=n_movies)
-tallies = [dict() for i in range(n_days)]
 
+# %%
 # %%
 def make_local_choices(user, items, local_count):
     local_choices = []
     dist = abs(items - user)
-    for i in range(local_count):
+    for _ in range(local_count):
         local_choices.append(dist.argmin())
         dist = np.delete(dist, dist.argmin())
     
@@ -26,13 +18,25 @@ def make_local_choices(user, items, local_count):
 
 def pl_percent(p, n):
     pl = stats.powerlaw.rvs(p, size=n)
-    pcts = [i/sum(pl) for i in pl]
+    pcts = sorted([i/sum(pl) for i in pl])
 
     return pcts
 
 # %%
+n_users = 1_000
+n_movies = 100
+n_days = 30
+pop_val = 0.5
+pl_val = 1.5
+
+users = stats.uniform.rvs(size=n_users)
+movies = stats.uniform.rvs(size=n_movies)
+tallies = [dict() for _ in range(n_days)]
+
+# %%
 for u in tqdm(users):
     choices = ((stats.binom.rvs(1, 1-pop_val, size=n_days)==1)).astype("object")
+    choices[choices==True] = "gap"
     local = np.count_nonzero(choices==0)
     local_choices = make_local_choices(u, movies, local)
     choices[choices==False] = np.array(local_choices)
@@ -44,11 +48,38 @@ for u in tqdm(users):
         else:
             tallies[i][k] = 1
 
-# %%
-init_pl = pl_percent(pl_val, n_movies)
+results = pd.DataFrame(tallies)
 
 # %%
-# Init power law distribution (converted to % chance)
-# Run n draws, n = number of gaps
+pl = pl_percent(pl_val, n_movies)
+for it in range(n_days):
+    gaps = results.loc[it, "gap"]
+    if it > 0:
+        p_notselected = 1 - (results[results.index<it].sum() / n_users)
+
+#    init = list(zip([i[0] for i in enumerate(movies)], pl))
+#    new_choices = np.random.choice([i[0] for i in init], gaps, p=[i[1] for i in init])
+
+# %%
+#- Init power law distribution (converted to % chance)
+#- Assign to items
+# Adjust based on prior selections
+#- Run n draws, n = number of gaps
 # Update counts
 # Create next power law distribution based on updated counts 
+
+# %%
+
+# %%
+df = pd.DataFrame(tallies)
+# %%
+df[df.index<20][38].sum()
+# %%
+
+# %%
+pl
+# %%
+it = results.iloc[1]
+# %%
+it[it.index!=True].index
+# %%
